747f8f93c7f95519ca114d861a4b62afae2410da
altaeva_al <altaeva_al@vk.com>
2024-10-03 18:21:41 +0300

    Commit #09 homework.kt

--- src/main/kotlin/com/JuliaToltaeva/kotlincourse/lesson9/@09 homework.kt
+++ src/main/kotlin/com/JuliaToltaeva/kotlincourse/lesson9/@09 homework.kt
@@ -1,13 +1,13 @@
 package com.juliatoltaeva.com.JuliaToltaeva.kotlincourse.lesson9
 
 fun main (){
-    BirthDay(150, "женат")
+    BirthDay(31, "не женат")
 }
 
 fun BirthDay(age: Int, status: String): String {
     println("Привет, Саша Молокович!")
     println("\uD83C\uDF89\uD83C\uDF89\uD83C\uDF89 С прошедшим днем рождения!!! \uD83C\uDF82\uD83C\uDF82\uD83C\uDF82")
-    println("\nТы утверждаешь, что твой возраст $age лет и статус <$status>.\n")
+    println("\nТы утверждаешь, что твой возраст $age лет и статус <$status>.")
 
     val ageWow50 = "Да ну нафиг, не может быть, что тебе столько лет!"
     val ageLess18 = "Ну и не говори правдивый возраст!"
@@ -24,8 +24,10 @@ fun BirthDay(age: Int, status: String): String {
             when {
                 status == "женат" -> {println("Еще раз поздравляю с Днюшей!")
                     println("Спроси у жены, пожалуйста, можно ли отправить тебе электронные обнимашки\uD83D\uDE05")
+                    println("Если можно, то мои тебе дистанционные обнимашки \uD83E\uDD17\uD83E\uDD17\uD83E\uDD17")
                 }
-                status == "не женат" -> println("Мои дистанционные обнимашки тебе \uD83E\uDD17\uD83E\uDD17\uD83E\uDD17")
+                status == "не женат" -> {println("Мои дистанционные обнимашки тебе \uD83E\uDD17\uD83E\uDD17\uD83E\uDD17")
+                println("Хорошей тебе жены! \uD83D\uDE00\uD83D\uDE00\uD83D\uDE00")}
                 else -> println("Так не бывает! Хитришь, ну ладно. Твоё право")
             }.toString()
         else -> ({println("\uD83D\uDE3E")}).toString()
