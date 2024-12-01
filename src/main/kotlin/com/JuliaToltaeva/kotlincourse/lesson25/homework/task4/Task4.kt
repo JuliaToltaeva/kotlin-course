87927fef9482c9568afde2bb3be2b87587528a27
altaeva_al <altaeva_al@vk.com>
2024-12-01 18:02:06 +0300

    #25 homework.kt

---
+++ src/main/kotlin/com/JuliaToltaeva/kotlincourse/lesson25/homework/task4/Task4.kt
@@ -0,0 +1,58 @@
+package com.juliatoltaeva.com.JuliaToltaeva.kotlincourse.lesson25.homework.task4
+
+import com.juliatoltaeva.com.JuliaToltaeva.kotlincourse.lesson25.homework.task3.func30
+import jdk.internal.org.jline.utils.Colors.s
+
+
+//Задание 4. Создай функцию, которая принимает большое число и возвращает сумму
+// цифр этого числа.
+//По аналогии с предыдущим заданием выполни трансформации и проверки.
+//Подсказка: для парсинга строки “3” в число 3 можно использовать “3”.digitToInt()
+
+fun func40(arg: String): Int {
+
+    require(arg.isNotEmpty()) { "Пустой список" }
+    return arg.sumOf { it.digitToInt() }
+
+}
+
+fun main() {
+
+    val func41 = fun(arg: String): Int {
+
+        require(arg.isNotEmpty()) { "Пустой список" }
+        return arg.sumOf { it.digitToInt() }
+
+    }
+
+    val func42: (String) -> Int = { arg ->
+
+        require(arg.isNotEmpty()) { "Пустой список" }
+        arg.sumOf { it.digitToInt() }
+
+    }
+
+    val func43 = { arg: String ->
+
+        require(arg.isNotEmpty()) { "Пустой список" }
+        arg.sumOf { it.digitToInt() }
+
+    }
+
+    println(func40("123"))
+    println(func41("123"))
+    println(func42("123"))
+    println(func43("123"))
+
+    println(func40("1"))
+    println(func41("1"))
+    println(func42("1"))
+    println(func43("1"))
+
+    println(func40(""))
+    println(func41(""))
+    println(func42(""))
+    println(func43(""))
+
+}
+
