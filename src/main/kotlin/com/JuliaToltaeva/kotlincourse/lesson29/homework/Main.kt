6c004738007123cc7ad1ff61b885b900911e0099
altaeva_al <altaeva_al@vk.com>
2024-12-14 18:03:47 +0300

    #29 homework.kt

--- src/main/kotlin/com/JuliaToltaeva/kotlincourse/lesson29/homework/Main.kt
+++ src/main/kotlin/com/JuliaToltaeva/kotlincourse/lesson29/homework/Main.kt
@@ -1,12 +1,15 @@
 package com.juliatoltaeva.com.JuliaToltaeva.kotlincourse.lesson29.homework
 
 import java.time.*
+import java.time.format.DateTimeFormatter
+import java.time.temporal.Temporal
 
 fun main() {
 
 //Создай текущую временнУю метку и выведи её на печать (чтобы ещё раз запомнить название этого класса)
 
     val instant = Instant.now()
+    println("Задача 1")
     println("Текущая временнАя метка")
     println(instant)
 
@@ -23,6 +26,8 @@ fun main() {
 //Создай период между датой своего рождения и текущей датой.
 //Выведи на печать количество лет из этого периода.
 
+    println("\nЗадача 3")
+
     val today = LocalDate.now()
     val period = Period.between(birthday, today)
 
@@ -33,12 +38,42 @@ fun main() {
 //Создай функцию, которая принимает тип Temporal и выводит форматированное значение в зависимости от того,
 //содержит ли аргумент время и часовой пояс. Temporal - это общий тип для разных классов даты-времени.
 //В форматированном значении нужно выводить часы, минуты, секунды и таймзону, если они представлены в переданном объекте.
-//Обработай в методе все типы дат, которые знаешь. Реализуй два варианта функции - с собственный форматированием
-//и с форматами из ISO коллекции.
+//Обработай в методе все типы дат, которые знаешь.
+//Реализуй два варианта функции - с собственный форматированием и с форматами из ISO коллекции.
 
-    val formateDate = fun(Temporal) {
+    println("\nЗадача 4")
+
+    val formatDate = fun(timeInner: Temporal): String {
+
+        val formatter = DateTimeFormatter.ofPattern(
+            "HH:mm:ss ZZZ"
+        )
+
+        return when (timeInner) {
+
+            is ZonedDateTime -> {
+                timeInner.format(formatter)
+            }
+
+            is Instant -> {
+                timeInner.toString()
+            }
+
+            else -> {
+                "Другой вид, которого я не привела?"
+            }
+        }
 
     }
+
+    val localDateTime = LocalDateTime.now()
+    val zonedDateTime = ZonedDateTime.now()
+    val offsetDateTime = OffsetDateTime.now()
+
+    println(formatDate(localDateTime))
+    println(formatDate(zonedDateTime))
+    println(formatDate(offsetDateTime))
+    println(formatDate(instant))
 
 //Создай объекты дат и / или времени которые мы изучили и по очереди передай их в метод созданный выше.
 //Не используй в них метод now()
