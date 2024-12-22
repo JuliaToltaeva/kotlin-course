10c4751122efa14fe1791a9ccd2fdc0389aff9ed
altaeva_al <altaeva_al@vk.com>
2024-12-22 18:30:06 +0300

    #31 homework.kt

---
+++ src/main/kotlin/com/JuliaToltaeva/kotlincourse/lesson31/homework/unittest/Cereal.kt
@@ -0,0 +1,9 @@
+package com.juliatoltaeva.com.JuliaToltaeva.kotlincourse.lesson31.homework.unittest
+
+enum class Cereal(local: String) {
+    BUCKWHEAT("Гречка"),
+    RICE("Рис"),
+    MILLET("Пшено"),
+    PEAS("Горох"),
+    BULGUR("Булгур")
+}
---
+++ src/main/kotlin/com/JuliaToltaeva/kotlincourse/lesson31/homework/unittest/CerealStorageImpl.kt
@@ -0,0 +1,53 @@
+package com.juliatoltaeva.com.JuliaToltaeva.kotlincourse.lesson31.homework.unittest
+
+// Пример имплементации интерфейса с блоком инициализации класса
+
+class CerealStorageImpl(
+    override val containerCapacity: Float,
+    override val storageCapacity: Float
+) : CerealStorage {
+
+
+    // Блок инициализации класса. Выполняется сразу при создании объекта
+    init {
+        require(containerCapacity >= 0) {
+            "Ёмкость контейнера не может быть отрицательной"
+        }
+        require(storageCapacity >= containerCapacity) {
+            "Ёмкость хранилища не должна быть меньше ёмкости одного контейнера"
+        }
+    }
+
+    private val storage = mutableMapOf<Cereal, Float>()
+
+    // дальше будет переопределением методов интерфейса
+
+    override fun addCereal(cereal: Cereal, amount: Float): Float {
+        TODO("Not yet implemented")
+    }
+
+    override fun getCereal(cereal: Cereal, amount: Float): Float {
+        TODO("Not yet implemented")
+    }
+
+    override fun removeContainer(cereal: Cereal): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun getAmount(cereal: Cereal): Float {
+        TODO("Not yet implemented")
+    }
+
+    override fun getSpace(cereal: Cereal): Float {
+        TODO("Not yet implemented")
+    }
+
+    override fun toString(): String {
+        TODO("Not yet implemented")
+    }
+
+
+
+
+
+}
