d47ce0c0f2d6ee3db66ab795502214189e51abe9
altaeva_al <altaeva_al@vk.com>
2024-12-22 18:32:27 +0300

    #31 homework.kt

---
+++ src/test/kotlin/com/JuliaToltaeva/kotlincourse/lesson31/homework/unittest/CerealStorageImplTest.kt
@@ -0,0 +1,34 @@
+package com.JuliaToltaeva.kotlincourse.lesson31.homework.unittest
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class CerealStorageImplTest {
+
+    @Test
+    fun addCereal(){
+
+    }
+
+    @Test
+    fun getCereal(){
+
+    }
+
+    @Test
+    fun removeContainer(){
+
+    }
+
+    @Test
+    fun getAmount(){
+
+    }
+
+    @Test
+    fun getSpace(){
+
+    }
+
+
+}
